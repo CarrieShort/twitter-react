25de2bd6babfb72b357625832391358aa546d816
bonecrushereb <bonecrushereb@gmail.com>
2016-05-16 10:50:17 -0700

    able to delete

--- app/js/tweets.jsx
+++ app/js/tweets.jsx
@@ -2,15 +2,32 @@ const React = require('react');
 const request = require('superagent');
 const baseUrl = 'http://localhost:3000';
 
-const RecentTweets = React.createClass({
+// const deletetweet = (id_str) => {
+//   console.log('delete button clicked!', id_str);
+// };
 
+const RecentTweets = React.createClass({
+  // handleClick: function(e) {
+  //   e.preventDefault();
+  //   var tweetId = this.props.tweet.str_id;
+  //   return this.props.onDelete(tweetId);
+  // },
   // get game info
   loadTwitterData: function() {
     this.serverRequest = request.get(baseUrl + '/api/recent_tweets').end(function(err, res) {
       var tweets = JSON.parse(res.text).data;
-      this.setState({tweets: tweets});
+      this.setState({tweets: tweets,});
     }.bind(this));
   },
+
+
+  // deleteTwitterData: function(id_str) {
+  //   console.log('delete button clicked!', id_str);
+  //   // this.serverRequest = request.del(baseUrl + '/api/remove_tweet/' + id_str).end(function(err, res) {
+  //   //   var tweets = JSON.parse(res.text).data
+  //   //   this.setState({tweets: tweets});
+  //   // }.bind(this));
+  // },
 
   getInitialState: function() {
     return {
@@ -34,13 +51,20 @@ const RecentTweets = React.createClass({
 });
 
 var TweetList = React.createClass({
-
+  handleDelete: function(e) {
+    var tweet = this.props.data[e.target.value];
+    var tweetId = tweet.id_str;
+    return this.serverRequest = request.del(baseUrl + '/api/remove_tweet/' + tweetId).end(function(err, res) {
+        var response = JSON.parse(res.text)
+        console.log('res.text', response.data, res);
+      }.bind(this));
+  },
   render: function() {
     return (
       <ul className="tweets">
-        {this.props.data.map(function(tweet) {
-          return <li key={tweet.text}>{tweet.text}</li>
-        })}
+        {this.props.data.map(function(tweet, tweetIndex) {
+          return <li key={tweetIndex}>{tweet.text}<button className="btn btn-default" tweet={tweet} onClick={this.handleDelete} value={tweetIndex}>Delete</button></li>
+        }.bind(this))}
       </ul>
     )
   }
--- rest_server.js
+++ rest_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const twitterRouter = require(__dirname + '/routes/twitter_router');
 
 app.use((req, res, next) => {
