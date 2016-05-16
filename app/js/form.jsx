f547e78b69e04b5bcfe7bba772d5c3a635ff3a84
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-15 18:40:55 -0700

    add webpack and setup basic react, bundle code, gulpfile

---
+++ app/js/form.jsx
@@ -0,0 +1,7 @@
+const React = require('react')
+module.exports = React.createClass({
+    displayName: 'HelloReact',
+    render: function(){
+        return <div>Hello React</div>
+    }
+});
---
+++ app/js/index.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require("react-dom");
+const Hello = require('./form.jsx')
+console.log(React);
+console.log(ReactDOM);
+ReactDOM.render(<Hello />, document.getElementById('content'));
